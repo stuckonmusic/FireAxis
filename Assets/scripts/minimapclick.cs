6498eabd71d315850effba28fbc3277e63df006a
stuckonmusic <prestonrichey@gmail.com>
2016-05-09 20:52:31 -0700

    updated

---
+++ Assets/scripts/minimapclick.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class minimapclick : MonoBehaviour {
+
+	public GameObject target
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
